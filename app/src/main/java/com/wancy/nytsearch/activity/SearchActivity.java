46a43b78b8a79c8140561e2c49de26714ccc7813
Wancy <flymydream2014@gmail.com>
2017-09-25 13:31:56 -0700

    Adding search view

--- app/src/main/java/com/wancy/nytsearch/activity/SearchActivity.java
+++ app/src/main/java/com/wancy/nytsearch/activity/SearchActivity.java
@@ -1,9 +1,11 @@
 package com.wancy.nytsearch.activity;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -60,14 +62,14 @@ public class SearchActivity extends AppCompatActivity implements EditDialogFragm
         params.put("page", String.valueOf(page));
 
         rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
-        etQuery = (EditText) findViewById(R.id.etQuery);
+/*        etQuery = (EditText) findViewById(R.id.etQuery);
         btnSearch = (Button) findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onArticleSearch(true);
             }
-        });
+        });*/
 
         articles = new ArrayList<>();
         articleAdapter = new ArticleAdapter(this, articles);
@@ -88,7 +90,25 @@ public class SearchActivity extends AppCompatActivity implements EditDialogFragm
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action if it is present.
         getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                onArticleSearch(true);
+                params.put("q", query);
+                searchView.clearFocus();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -120,11 +140,6 @@ public class SearchActivity extends AppCompatActivity implements EditDialogFragm
     }
 
     public void onArticleSearch(final Boolean isNewSearch) {
-        String query = etQuery.getText().toString();
-        //if (query == null || query.length() == 0) return;
-        Toast.makeText(this, "Searching for " + query, Toast.LENGTH_LONG).show();
-
-        params.put("q", query);
         String url = "";
         for (String key : params.keySet()) {
             String paramString = params.get(key).trim();
